a9074b8088398eaee0a247bf0e40995740a525b2
Caleb Evans <caleb@calebevans.me>
2023-12-17 21:04:24 -0800

    Fix label/input associations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,19 +15,24 @@
 
 	<form class="passwords">
 		<div class="reveal">
-			<label for="reveal">Reveal passwords?</label>
-			<input class="reveal" type="checkbox" value="" />
+			<label for="reveal-passwords">Reveal passwords?</label>
+			<input id="reveal-passwords" class="reveal" type="checkbox" value="" />
 		</div>
 
 		<div class="field password">
 			<label for="password">Password:</label>
-			<input type="password" placeholder="Type password" class="password" />
+			<input type="password" placeholder="Type password" id="password" class="password" />
 			<span class="password-length"></span>
 		</div>
 
 		<div class="field confirm">
-			<label for="confirm">Confirm:</label>
-			<input type="password" placeholder="Confirm password" class="password confirm" />
+			<label for="confirm-password">Confirm:</label>
+			<input
+				type="password"
+				placeholder="Confirm password"
+				id="confirm-password"
+				class="password confirm"
+			/>
 			<span class="confirm-length"></span>
 		</div>
 

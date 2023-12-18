451409067ad5a122ad00c74d3bc1a04dad300035
Caleb Evans <caleb@calebevans.me>
2023-12-18 14:06:42 -0800

    Switch password inputs when pressing Enter key

--- src/routes/Password.svelte
+++ src/routes/Password.svelte
@@ -12,7 +12,25 @@
 	// simply listen for the input's `input` event and update the variable
 	// accordingly
 	function updateValue(event: Event & { currentTarget: EventTarget & HTMLInputElement }) {
-		value = event.currentTarget?.value ?? '';
+		value = event.currentTarget.value ?? '';
+	}
+	// Switch focus to the other password input on the page when the Enter key
+	// is pressed; this is handled in an element-agnostic way such that we don't
+	// need a reference to the DOM element ahead of time, now do we need to
+	// query it with an arbitrary selector - we can just leverage the DOM API
+	function switchInputsOnEnter(
+		event: KeyboardEvent & { currentTarget: EventTarget & HTMLInputElement }
+	) {
+		if (event.key !== 'Enter') {
+			return;
+		}
+		const formElements = Array.from(event.currentTarget.form?.elements ?? []) as HTMLInputElement[];
+		const nextField = formElements.find((element) => {
+			return element.hasAttribute('data-toggle-focus-via-enter') && element !== event.currentTarget;
+		});
+		// Focus the input and select its contents so the user can type their
+		// password from the beginning again
+		nextField?.select();
 	}
 </script>
 
@@ -23,6 +41,8 @@
 	{placeholder}
 	{id}
 	on:input={(event) => updateValue(event)}
+	on:keydown={switchInputsOnEnter}
+	data-toggle-focus-via-enter
 	data-1p-ignore
 />
 <span class="password-length">{value.length || ''}</span>

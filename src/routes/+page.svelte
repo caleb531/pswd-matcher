39cded08955590ad6ccc49459624de279e08d401
Caleb Evans <caleb@calebevans.me>
2023-12-17 20:50:12 -0800

    Migrate over markup and styles from old app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '../styles/index.scss';
+</script>
+
+<main class="app">
+	<h1>pswd matcher</h1>
+
+	<p>Need to learn a new password? Learn it here!</p>
+
+	<form class="passwords">
+		<div class="reveal">
+			<label for="reveal">Reveal passwords?</label>
+			<input class="reveal" type="checkbox" value="" />
+		</div>
+
+		<div class="field password">
+			<label for="password">Password:</label>
+			<input type="password" placeholder="Type password" class="password" />
+			<span class="password-length"></span>
+		</div>
+
+		<div class="field confirm">
+			<label for="confirm">Confirm:</label>
+			<input type="password" placeholder="Confirm password" class="password confirm" />
+			<span class="confirm-length"></span>
+		</div>
+
+		<div class="match-container">
+			<p class="match status visible">Passwords match</p>
+			<p class="no-match status">Passwords do not match</p>
+		</div>
+	</form>
+
+	<p><strong>Disclaimer:</strong> Nothing you type here is sent or stored anywhere. Period.</p>
+</main>

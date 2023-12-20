15efabfb9be682204473da1723c0e0d7c9edf0a6
Caleb Evans <caleb@calebevans.me>
2023-12-20 14:43:38 -0800

    Improve accessibility of match reading

--- src/routes/MatchStatus.svelte
+++ src/routes/MatchStatus.svelte
@@ -4,13 +4,13 @@
 </script>
 
 <div
-	class="match-container"
-	class:visible={Boolean(initialPassword !== '' && confirmPassword !== '')}
+	class="match-status-container"
+	aria-hidden={!(initialPassword !== '' && confirmPassword !== '')}
+	aria-live="polite"
 >
-	<p class="match-status matching" class:visible={initialPassword === confirmPassword}>
-		Passwords match
-	</p>
-	<p class="match-status not-matching" class:visible={initialPassword !== confirmPassword}>
-		Passwords do not match
-	</p>
+	{#if initialPassword === confirmPassword}
+		<p class="match-status matching">Passwords match</p>
+	{:else}
+		<p class="match-status not-matching">Passwords do not match</p>
+	{/if}
 </div>

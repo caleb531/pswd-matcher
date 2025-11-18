4994436e9d68dec02c11192eacdb72ed6b4ef743
Caleb Evans <caleb@calebevans.me>
2025-11-18 12:55:01 -0800

    Upgrade vitest to latest

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
 import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 
-export default defineConfig(({ mode }) => ({
-	plugins: [sveltekit()],
-	// Fix vitest under Svelte 5, per
-	// <https://github.com/sveltejs/svelte/issues/11394#issuecomment-2085747668>
-	...(mode === 'test'
-		? {
-				resolve: {
-					conditions: mode === 'test' ? ['browser'] : []
-				}
-			}
-		: {}),
-	// Vitest does not officially support Vite 6 as of yet, and if we try to
-	// upgrade, we get TypeScript errors loading the SvelteKit plugin; to work
-	// around this, the application must remain on Vite 5 for the time being
-	// (until Vitest officially supports Vite 6); with that, we must enable the
-	// use of the modern Sass API in Vite 5
-	css: {
-		preprocessorOptions: {
-			scss: {
-				api: 'modern-compiler'
-			}
-		}
-	},
+export default defineConfig({
+	plugins: [sveltekit(), svelteTesting()],
 	test: {
 		environment: 'jsdom',
 		globals: true,
@@ -33,4 +13,4 @@ export default defineConfig(({ mode }) => ({
 			reporter: ['text', 'lcov', 'html', 'text-summary']
 		}
 	}
-}));
+});
